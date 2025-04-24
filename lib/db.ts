f43a454251215a192c9afee7a219d063bc1837fc
wlcao <2674754050@qq.com>
2025-04-24 21:32:50 +0800

    重构数据库配置和用户模型，更新环境变量，移除不必要的代码

---
+++ lib/db.ts
@@ -0,0 +1,22 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import { eq } from "drizzle-orm";
+import postgres from "postgres";
+import { genSaltSync, hashSync } from "bcrypt-ts";
+import { users } from "./schema";
+
+// Optionally, if not using email/pass login, you can
+// use the Drizzle adapter for Auth.js / NextAuth
+// https://authjs.dev/reference/adapter/drizzle
+let client = postgres(process.env.DATABASE_URL!);
+
+export const db = drizzle(client);
+export async function getUser(email: string) {
+  return await db.select().from(users).where(eq(users.email, email));
+}
+
+export async function createUser(email: string, password: string) {
+  let salt = genSaltSync(10);
+  let hash = hashSync(password, salt);
+
+  return await db.insert(users).values({ email, password: hash });
+}
